93cce7949674940ecc99b3d6f6dfa0bcbd39522e
Saha5588 <mgacode3@gmail.com>
2021-05-17 22:01:23 +0530

    new_committ

--- Functions.py
+++ Functions.py
@@ -11,6 +11,9 @@
 # after everything getting intdented we can print the funtiom and call it back by
 # calling the name of the function and it wll print the string
 #
+from random import shuffle
+
+
 def say_hello():
     print("hello")
 
@@ -140,3 +143,34 @@ name, hours = employee_check(work_hours)
 print(result)
 print(name)
 print(hours)
+
+
+# INTERACTIONS BETWEEN PYTHON FUNCTIONS
+my_list = ['', 'o', '']
+
+
+def shuffle_list(my_list):
+    shuffle(my_list)
+
+    return my_list
+
+
+def player_guess():
+    guess = ''
+    while guess not in ['0', '1', '2']:
+        guess = (input("please choose a number: 0,1 or 2"))
+    return int(guess)
+
+
+def check_guess(my_list, guess):
+    if my_list[guess] == 'o':
+        print('correct guess!')
+    else:
+        print('wrong! better luck next time')
+        print(my_list)
+
+
+my_list = ['', 'o', '']
+mixedup_list = shuffle_list(my_list)
+guess = player_guess()
+print(check_guess(mixedup_list, guess))
