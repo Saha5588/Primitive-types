01ba236548030f300fbd5c2f283e39d22acd5063
Saha5588 <mgacode3@gmail.com>
2021-05-23 18:45:32 +0530

    pt_commit

--- pt.py
+++ pt.py
@@ -1,2 +1,29 @@
-print("hello worl")
-print("Madhurjya Saha")
+# INTERACTIONS BETWEEN PYTHON FUNCTIONS
+my_list = ['', 'o', '']
+
+
+def shuffle_list(my_list):
+    shuffle(my_list)
+
+    return my_list
+
+
+def player_guess():
+    guess = ''
+    while guess not in ['0', '1', '2']:
+        guess = (input("please choose a number: 0,1 or 2"))
+    return int(guess)
+
+
+def check_guess(my_list, guess):
+    if my_list[guess] == 'o':
+        print('correct guess!')
+    else:
+        print('wrong! better luck next time')
+        print(my_list)
+
+
+my_list = ['', 'o', '']
+mixedup_list = shuffle_list(my_list)
+guess = player_guess()
+print(check_guess(mixedup_list, guess))
